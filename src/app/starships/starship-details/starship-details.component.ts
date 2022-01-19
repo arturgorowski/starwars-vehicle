6ef8a594628a68ac93e9c7633855384e52fbd66c
arturgorowski <art.gorowski@gmail.com>
2022-01-19 22:07:41 +0100

    add starship details component

--- src/app/starships/starship-details/starship-details.component.ts
+++ src/app/starships/starship-details/starship-details.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Starships} from "../../_models/starships";
+import {ActivatedRoute} from "@angular/router";
+import {StarshipStorage} from "../../_helpers/starship-storage";
 
 @Component({
-  selector: 'app-starship-details',
-  templateUrl: './starship-details.component.html',
-  styleUrls: ['./starship-details.component.scss']
+    selector: 'app-starship-details',
+    templateUrl: './starship-details.component.html',
+    styleUrls: ['./starship-details.component.scss']
 })
 export class StarshipDetailsComponent implements OnInit {
 
-  constructor() { }
+    starship: Starships;
+    constructor(protected route: ActivatedRoute) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.loadStarshipDetails();
+    }
 
+    loadStarshipDetails() {
+        this.starship = this.route.snapshot.data['starshipDetails'];
+    }
+
+    addToCompare(starship: Starships) {
+        StarshipStorage.addStarship(starship);
+    }
 }
