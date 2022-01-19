849b6e53213bc7c8f0ef22eb8edd0c0b00dd6beb
arturgorowski <art.gorowski@gmail.com>
2022-01-19 23:06:13 +0100

    add starship behaviour subject

---
+++ src/app/_services/starship.service.ts
@@ -0,0 +1,36 @@
+import {BehaviorSubject, Observable} from "rxjs";
+import {Starships} from "../_models/starships";
+import {StarshipStorage} from "../_helpers/starship-storage";
+import {Injectable} from "@angular/core";
+
+@Injectable({
+    providedIn: "root"
+})
+export class StarshipService {
+
+    private starships: BehaviorSubject<Starships[]> = new BehaviorSubject<Starships[]>([]);
+    cast$: Observable<Starships[]> = this.starships.asObservable();
+
+    constructor() {
+        this.loadStarships();
+    }
+
+    private loadStarships() {
+        const starships: Starships[] = StarshipStorage.getStarships();
+        console.log(starships);
+        if (starships.length) {
+            this.starships.next(starships);
+        }
+    }
+
+    addStarship(starships: Starships): void {
+        StarshipStorage.addStarship(starships);
+        this.starships.next(StarshipStorage.getStarships());
+    }
+
+    removeStarship(starships: Starships): void {
+        StarshipStorage.removeStarship(starships);
+        this.starships.next(StarshipStorage.getStarships());
+    }
+
+}
